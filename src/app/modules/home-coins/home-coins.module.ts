65886d2d6aba1baa1c4e576757e027b56a559287
Cibele Martins <cibelemartins24@hotmail.com>
2023-03-05 13:19:16 -0300

    🚧 feat: removeu os exports de home-coins-module

--- src/app/modules/home-coins/home-coins.module.ts
+++ src/app/modules/home-coins/home-coins.module.ts
@@ -26,16 +26,25 @@ import { HomeCoinsRoutingModule } from './home-coins-routing.module';
         // SharedModule
         CommonModule,
         FormsModule,
-        HomeCoinsRoutingModule //o módulo de rotas de home-coins
+        HomeCoinsRoutingModule //o módulo de rotas de rotas de home-coins
 
     ],
-    exports: [ValueCoinsComponent,
-        CryptoInfosComponent,
-        GraphicComponent,
-        ConversionDashboardComponent,
-        SpinnerComponent,
-        ConvertActionComponent,
-        HomeCoinsComponent] //assim, qualquer módulo que importe o módulo home-coins poderá utilizar estes componentes
+    exports: 
+    [
+        // ValueCoinsComponent,
+        // CryptoInfosComponent,
+        // GraphicComponent,
+        // ConversionDashboardComponent,
+        // SpinnerComponent,
+        // ConvertActionComponent,
+        // HomeCoinsComponent
+    ] //assim, qualquer módulo que importe o módulo home-coins poderá utilizar estes componentes
+
+    // depois de definir o módulo de roteamento de home-coins-module, já n"ao é mais necessário exportar estes componentes,
+    // porque a rota de HomeCoinsComponent que o utiliza está fundindo-se com a rota raizz da aplicação
+    // com a utilização do método forChild(), assim, esses componentes anes exportados não são mais utilizados 
+    // em nenhum outro lugar da aplicação, sem a necessidade de exportá-los então
+   
 })
 export class HomeCoinsModule {
 
